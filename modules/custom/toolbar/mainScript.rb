b4336f647b0ddfe02f15a250992ae4b6cd087e6a
TheGetHuber <reidolf.danila@gmail.com>
2023-12-13 00:28:39 +0300

    toolbar addiction

--- modules/custom/toolbar/mainScript.rb
+++ modules/custom/toolbar/mainScript.rb
@@ -1,9 +1,12 @@
 class Toolbar < BaseModule
     def initialize(*args)
         super
+
+        @userInput
+        @termux = @core.getModule("TermuxAPI")
     end
 
     def mainLoop()
-        
+        @termux.notification("test", "notification")
     end
 end
